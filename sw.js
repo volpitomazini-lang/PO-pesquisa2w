3bd53a9184f1a22bfa4c78b2344add4dddd9506c
volpitomazini-lang <volpi.tomazini@gmail.com>
2026-01-15 22:27:06 -0300

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,7 +1,36 @@
+const CACHE_NAME = 'peo-cache-v1';
+const assets = [
+  './',
+  './index.html',
+  './manifest.json'
+];
+
+// Instala o Service Worker e armazena os arquivos no cache
 self.addEventListener('install', (event) => {
-  console.log('P&O Service Worker instalado');
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      console.log('P e O: Arquivos em cache');
+      return cache.addAll(assets);
+    })
+  );
 });
 
+// Responde com os arquivos do cache ou busca na rede se necessário
 self.addEventListener('fetch', (event) => {
-  // Necessário para que o navegador permita a instalação
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request);
+    })
+  );
+});
+
+// Ativa o novo Service Worker e limpa caches antigos se houver
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then((cacheNames) => {
+      return Promise.all(
+        cacheNames.filter((name) => name !== CACHE_NAME).map((name) => caches.delete(name))
+      );
+    })
+  );
 });
