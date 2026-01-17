98c19274731ce36ce5166c1db9f5e4bc0e10c3ec
volpitomazini-lang <volpi.tomazini@gmail.com>
2026-01-17 07:55:33 -0300

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,3 +1,14 @@
-self.options = { "domain": "3nbf4.com", "zoneId": 10473815 };
-self.lary = "";
-importScripts('https://3nbf4.com/act/files/service-worker.min.js?r=sw');
+const CACHE_NAME = 'po-cache-v1';
+const urlsToCache = ['/'];
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(urlsToCache))
+  );
+});
+
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => response || fetch(event.request))
+  );
+});
